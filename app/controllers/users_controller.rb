aafb630bd1e8cbef41cc21cd1ecd4c6dfb318bdd
toshiroriki <toshiroriki@icloud.com>
2021-08-01 12:59:09 +0000

    finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
  @user = User.find(params[:id])
  @book = Book.new
+ @books = @user.books
   end
 
   def index
@@ -12,6 +13,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
   def update
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'homes#top'
+  root to: 'homes#top'
  resources :users
  resources :books
- get 'about', to: 'homes#about'
+ get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
